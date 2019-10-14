e491c1d45f6737d2492c7b350135e0e20daa0abd
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-10-14 23:13:07 +0300

    Add canvas

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,14 +1,29 @@
-window.onload = function () {
-    new Vue({
+window.onload = () => {
+    var vue = new Vue({
         el: '#vue-app',
         data: {
             name: 'Kondio',
-            job: 'Singer'
+            job: 'Singer',
+            website: 'https://google.com',
+            websiteTag: '<a href="https://google.com">Tuka nqma</a>',
+            age: 22,
+            x: 0,
+            y: 0
         },
         methods: {
-            greet: (timeOfDay) => {
-                return `Greetings ${timeOfDay}`;
+            greet: function () {
+                return `Greetings ${this.name}`;
+            },
+            add: function() {
+                this.age++;
+            },
+            subtract: function() {
+                this.age--;
+            },
+            updateMouse: function(event) {
+                this.x = event.offsetX;
+                this.y = event.offsetY;
             }
         }
-    });
-}
+     });
+};
