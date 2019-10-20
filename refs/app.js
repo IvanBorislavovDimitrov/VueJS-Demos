3fe7ce430d7c8ae482087e6ced86c532ba88a9e6
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2019-10-20 21:43:28 +0300

    Add refs

--- refs/app.js
+++ refs/app.js
@@ -2,10 +2,13 @@ window.onload = () => {
     var vue = new Vue({
         el: '#vue-app',
         data: {
-
+            output: ''
         },
         methods: {
-
+            readUsername: function() {
+                console.log(this.$refs.username.value);
+                this.output = this.$refs.username.value;
+            }
         }
      });
 };
